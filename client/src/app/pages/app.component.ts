f9031351ecc501d99c7092947c6951fbf7e5945f
Niknikson <niknikson34@gmail.com>
2021-09-13 13:27:16 +0300

    feat: add nav menu

--- client/src/app/pages/app.component.ts
+++ client/src/app/pages/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  menu: string = 'active'
+  menu: string = ''
   
   ngOnInit(): void {
    
--- client/src/app/pages/app.module.ts
+++ client/src/app/pages/app.module.ts
@@ -2,21 +2,26 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClientPageComponent } from './client-page/client-page/client-page.component';
-import { AdminPageComponent } from './admin-page/admin-page/admin-page.component';
-import { HeaderComponent } from '../components/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from '../features/menu/menu.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { MenuItemsComponent } from '../features/menu-items/menu-items.component';
+import { AdminPageComponent } from './admin-page/admin-page/admin-page.component';
+import { ClientPageComponent } from './client-page/client-page/client-page.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuComponent,
     HeaderComponent,
     FooterComponent,
-    ClientPageComponent,
+    MenuItemsComponent,
     AdminPageComponent,
+    ClientPageComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
