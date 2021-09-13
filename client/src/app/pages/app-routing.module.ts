e07b84675eebba8881e82a5d41ebf3595ba81819
Niknikson <niknikson34@gmail.com>
2021-09-13 10:24:01 +0300

    feat: add pages and burger button

--- client/src/app/pages/app-routing.module.ts
+++ client/src/app/pages/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPageComponent } from './admin-page/admin-page/admin-page.component';
+import { ClientPageComponent } from './client-page/client-page/client-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ClientPageComponent },
+  { path: 'admin', component: AdminPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- client/src/app/pages/app.module.ts
+++ client/src/app/pages/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientPageComponent } from './client-page/client-page/client-page.component';
+import { AdminPageComponent } from './admin-page/admin-page/admin-page.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ClientPageComponent,
+    AdminPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
